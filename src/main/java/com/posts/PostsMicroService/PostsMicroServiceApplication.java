109275de961e9cb4ed2a9d2715f8ac68002bb1b8
Rishabh Goel <rishu@Rishus-MacBook-Pro.local>
2019-01-28 18:45:59 +0530

    PostsMicroService Project Setup

---
+++ src/main/java/com/posts/PostsMicroService/PostsMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.posts.PostsMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsMicroServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/posts/PostsMicroService/PostsMicroServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.posts.PostsMicroService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsMicroServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
