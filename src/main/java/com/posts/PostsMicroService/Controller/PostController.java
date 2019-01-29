a2ab13b1ecdf4d3b0ecabbd4e06593a53c930a8b
akshita <akshim2002@gmail.com>
2019-01-29 20:18:55 +0530

    feedCommit

--- src/main/java/com/posts/PostsMicroService/Controller/PostController.java
+++ src/main/java/com/posts/PostsMicroService/Controller/PostController.java
@@ -67,7 +67,6 @@ public class PostController {
         try {
             newPost.setUserId(userId);
             newPost.setDate();
-
             newPost.setCreatedBy(sourcePost.getCreatedBy());
             newPost.setDescription(sourcePost.getDescription());
 
